906f47d627395bbf58549fb298a291e5564f8f86
buba71 <buba71@sfr.fr>
2017-05-08 23:45:49 +0200

    création des controllers

--- app/routes.php
+++ app/routes.php
@@ -1,179 +1,65 @@
 <?php
 
-
-use Symfony\Component\HttpFoundation\Request;
-use OCBlog\Domain\Comment;
-use OCBlog\Domain\Article;
-use OCBlog\Domain\User;
-use OCBlog\Form\Type\CommentType;
-use OCBlog\Form\Type\ArticleType;
-use OCBlog\Form\Type\UserType;
-
 // Home page
-$app->get('/', function () use ($app) {
-    $articles = $app['dao.article']->findAll();
-    return $app['twig']->render('index.html.twig', array('articles' => $articles));
-})->bind('home');
+$app->get('/', "OCBlog\Controller\HomeController::indexAction")
+->bind('home');
 
-// Admin home page
-$app->get('/admin', function() use ($app) {
-    $articles = $app['dao.article']->findAll();
-    $comments = $app['dao.comment']->findAll();
-    $users = $app['dao.user']->findAll();
-    return $app['twig']->render('admin.html.twig', array(
-        'articles' => $articles,
-        'comments' => $comments,
-        'users' => $users));
-})->bind('admin');
-
-// Article details with comments
-$app->match('/article/{id}', function ($id, Request $request) use ($app) {
-    $article = $app['dao.article']->find($id);
-    $commentFormView = null;
-    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
-    	// User is authenticated : he can add comments
-    	$comment =new Comment();
-    	$comment->setArticle($article);
-    	$user = $app['user'];
-    	$comment->setAuthor($user);
-    	$commentForm = $app['form.factory']->create(CommentType::class, $comment);
-    	$commentForm->handleRequest($request);
-    	if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-    			$app['dao.comment']->save($comment);
-    			$app['session']->getFlashBag()->add('success', 'Your comment was succefully added ! ');
-    	}
-
-    	$commentFormView = $commentForm->createView();
-    }
-
-    $comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('article.html.twig', array(
-    	'article' => $article,
-        'comments' => $comments,
-        'commentForm' => $commentFormView));
-})->bind('article');
-
-// Add a new article
-$app->match('/admin/article/add', function(Request $request) use ($app) {
-    $article = new Article();
-    $articleForm = $app['form.factory']->create(ArticleType::class, $article);
-    $articleForm->handleRequest($request);
-    if ($articleForm->isSubmitted() && $articleForm->isValid()) {
-        $app['dao.article']->save($article);
-        $app['session']->getFlashBag()->add('success', 'The article was successfully created.');
-    }
-    return $app['twig']->render('article_form.html.twig', array(
-        'title' => 'New article',
-        'articleForm' => $articleForm->createView()));
-})->bind('admin_article_add');
-
-// Edit an existing article
-$app->match('/admin/article/{id}/edit', function($id, Request $request) use ($app) {
-    $article = $app['dao.article']->find($id);
-    $articleForm = $app['form.factory']->create(ArticleType::class, $article);
-    $articleForm->handleRequest($request);
-    if ($articleForm->isSubmitted() && $articleForm->isValid()) {
-        $app['dao.article']->save($article);
-        $app['session']->getFlashBag()->add('success', 'The article was successfully updated.');
-    }
-    return $app['twig']->render('article_form.html.twig', array(
-        'title' => 'Edit article',
-        'articleForm' => $articleForm->createView()));
-})->bind('admin_article_edit');
-
-// Remove an article
-$app->get('/admin/article/{id}/delete', function($id, Request $request) use ($app) {
-    // Delete all associated comments
-    $app['dao.comment']->deleteAllByArticle($id);
-    // Delete the article
-    $app['dao.article']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'The article was successfully removed.');
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_article_delete');
-
-// Edit an existing comment
-$app->match('/admin/comment/{id}/edit', function($id, Request $request) use ($app) {
-    $comment = $app['dao.comment']->find($id);
-    $commentForm = $app['form.factory']->create(CommentType::class, $comment);
-    $commentForm->handleRequest($request);
-    if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-        $app['dao.comment']->save($comment);
-        $app['session']->getFlashBag()->add('success', 'The comment was successfully updated.');
-    }
-    return $app['twig']->render('comment_form.html.twig', array(
-        'title' => 'Edit comment',
-        'commentForm' => $commentForm->createView()));
-})->bind('admin_comment_edit');
-
-// Remove a comment
-$app->get('/admin/comment/{id}/delete', function($id, Request $request) use ($app) {
-    $app['dao.comment']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'The comment was successfully removed.');
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_comment_delete');
-
-
-// Add a user
-$app->match('/admin/user/add', function(Request $request) use ($app) {
-    $user = new User();
-    $userForm = $app['form.factory']->create(UserType::class, $user);
-    $userForm->handleRequest($request);
-    if ($userForm->isSubmitted() && $userForm->isValid()) {
-        // generate a random salt value
-        $salt = substr(md5(time()), 0, 23);
-        $user->setSalt($salt);
-        $plainPassword = $user->getPassword();
-        // find the default encoder
-        $encoder = $app['security.encoder.bcrypt'];
-        // compute the encoded password
-        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
-        $user->setPassword($password); 
-        $app['dao.user']->save($user);
-        $app['session']->getFlashBag()->add('success', 'The user was successfully created.');
-    }
-    return $app['twig']->render('user_form.html.twig', array(
-        'title' => 'New user',
-        'userForm' => $userForm->createView()));
-})->bind('admin_user_add');
-
-// Edit an existing user
-$app->match('/admin/user/{id}/edit', function($id, Request $request) use ($app) {
-    $user = $app['dao.user']->find($id);
-    $userForm = $app['form.factory']->create(UserType::class, $user);
-    $userForm->handleRequest($request);
-    if ($userForm->isSubmitted() && $userForm->isValid()) {
-        $plainPassword = $user->getPassword();
-        // find the encoder for the user
-        $encoder = $app['security.encoder_factory']->getEncoder($user);
-        // compute the encoded password
-        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
-        $user->setPassword($password); 
-        $app['dao.user']->save($user);
-        $app['session']->getFlashBag()->add('success', 'The user was successfully updated.');
-    }
-    return $app['twig']->render('user_form.html.twig', array(
-        'title' => 'Edit user',
-        'userForm' => $userForm->createView()));
-})->bind('admin_user_edit');
-
-// Remove a user
-$app->get('/admin/user/{id}/delete', function($id, Request $request) use ($app) {
-    // Delete all associated comments
-    $app['dao.comment']->deleteAllByUser($id);
-    // Delete the user
-    $app['dao.user']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'The user was successfully removed.');
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_user_delete');
-
+// Detailed info about an article
+$app->match('/article/{id}', "OCBlog\Controller\HomeController::articleAction")
+->bind('article');
 
 // Login form
-$app->get('/login', function (Request $request) use ($app) {
-	return $app['twig']->render('login.html.twig', array(
-		'error'			=> $app['security.last_error']($request),
-		'last_username' => $app['session']->get('_security.last_username'),
-	));
-})->bind('login');
+$app->get('/login', "OCBlog\Controller\HomeController::loginAction")
+->bind('login');
+
+// Admin zone
+$app->get('/admin', "OCBlog\Controller\AdminController::indexAction")
+->bind('admin');
+
+// Add a new article
+$app->match('/admin/article/add', "OCBlog\Controller\AdminController::addArticleAction")
+->bind('admin_article_add');
+
+// Edit an existing article
+$app->match('/admin/article/{id}/edit', "OCBlog\Controller\AdminController::editArticleAction")
+->bind('admin_article_edit');
+
+// Remove an article
+$app->get('/admin/article/{id}/delete', "OCBlog\Controller\AdminController::deleteArticleAction")
+->bind('admin_article_delete');
+
+// Edit an existing comment
+$app->match('/admin/comment/{id}/edit', "OCBlog\Controller\AdminController::editCommentAction")
+->bind('admin_comment_edit');
+
+// Remove a comment
+$app->get('/admin/comment/{id}/delete', "OCBlog\Controller\AdminController::deleteCommentAction")
+->bind('admin_comment_delete');
+
+// Add a user
+$app->match('/admin/user/add', "OCBlog\Controller\AdminController::addUserAction")
+->bind('admin_user_add');
+
+// Edit an existing user
+$app->match('/admin/user/{id}/edit', "OCBlog\Controller\AdminController::editUserAction")
+->bind('admin_user_edit');
+
+// Remove a user
+$app->get('/admin/user/{id}/delete', "OCBlog\Controller\AdminController::deleteUserAction")
+->bind('admin_user_delete');
+
+// API : get all articles
+$app->get('/api/articles', "OCBlog\Controller\ApiController::getArticlesAction")
+->bind('api_articles');
+
+// API : get an article
+$app->get('/api/article/{id}', "OCBlog\Controller\ApiController::getArticleAction")
+->bind('api_article');
+
+// API : create an article
+$app->post('/api/article', "OCBlog\Controller\ApiController::addArticleAction")
+->bind('api_article_add');
+
+// API : remove an article
+$app->delete('/api/article/{id}', "OCBlog\Controller\ApiController::deleteArticleAction")
+->bind('api_article_delete');
