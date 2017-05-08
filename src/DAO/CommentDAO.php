502ac16b705c829824b1eca09316be9dbec06b30
buba71 <buba71@sfr.fr>
2017-05-08 09:09:32 +0200

    itération 8

--- src/DAO/CommentDAO.php
+++ src/DAO/CommentDAO.php
@@ -4,15 +4,27 @@ namespace OCBlog\DAO;
 
 use OCBlog\Domain\Comment;
 
-class CommentDAO extends DAO 
+class CommentDAO extends DAO
 {
     /**
      * @var \OCBlog\DAO\ArticleDAO
      */
     private $articleDAO;
 
+    /**
+     *
+     * @var \OCBlog\DAO\UserDAO
+     */
+    private $userDAO;
+
     public function setArticleDAO(ArticleDAO $articleDAO) {
         $this->articleDAO = $articleDAO;
+    }
+
+    public function setUserDAO(UserDAO $userDAO)
+    {
+        $this->userDAO = $userDAO;
+        
     }
 
     /**
@@ -28,7 +40,7 @@ class CommentDAO extends DAO
 
         // art_id is not selected by the SQL query
         // The article won't be retrieved during domain objet construction
-        $sql = "SELECT com_id, com_content, com_author from comment where art_id=? order by com_id";
+        $sql = "SELECT com_id, com_content, usr_id from comment where art_id=? order by com_id";
         $result = $this->getDb()->fetchAll($sql, array($articleId));
 
         // Convert query result to an array of domain objects
@@ -53,13 +65,20 @@ class CommentDAO extends DAO
         $comment = new Comment();
         $comment->setId($row['com_id']);
         $comment->setContent($row['com_content']);
-        $comment->setAuthor($row['com_author']);
+        
 
         if (array_key_exists('art_id', $row)) {
             // Find and set the associated article
             $articleId = $row['art_id'];
             $article = $this->articleDAO->find($articleId);
             $comment->setArticle($article);
+        }
+
+        if (array_key_exists('usr_id', $row)) {
+            // Find and set the associated author
+            $userId =$row['usr_id'];
+            $user =$this->userDAO->find($userId);
+            $comment = setAuthor($user);
         }
         
         return $comment;
