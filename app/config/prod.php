1ad128683d3dc68f7e6e83cd803a6db9b6db3c4f
buba71 <buba71@sfr.fr>
2017-08-20 12:33:15 +0200

    local prod vers

--- app/config/prod.php
+++ app/config/prod.php
@@ -5,9 +5,9 @@
 $app['db.options'] = array(
 	'driver'	=> 'pdo_mysql',
 	'charset'	=> 'utf8',
-	'host'		=> 'db690983358.db.1and1.com',
+	'host'		=> 'localhost',
 	'port'		=> '3306',
-	'dbname'	=> 'db690983358',
-	'user'		=> 'dbo690983358',
-	'password'	=> 'Yqpkaqrv1',
+	'dbname'	=> 'OCblog',
+	'user'		=> 'ocblog_user',
+	'password'	=> 'david',
 	);
